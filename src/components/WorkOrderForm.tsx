6f1052fb860f3ef0de8ae208d2cf105c81579cbe
tomcat955 <Tombruzan@tcbmetalworks.com>
2025-03-27 00:29:59 -0500

    Fix type error in WorkOrderForm

--- src/components/WorkOrderForm.tsx
+++ src/components/WorkOrderForm.tsx
@@ -47,7 +47,7 @@ export default function WorkOrderForm() {
       },
       consultation: {
         description: '',
-        preferredContactMethod: '',
+        preferredContactMethod: undefined,
         bestTimeToContact: '',
       },
       equipmentService: {
